870ab19fbd605df34ad00f0376efdbc7756a1c38
RJ Mangubat <mangubatrj@gmail.com>
2020-08-21 13:50:41 +0800

    Add Constants.kt

---
+++ app/src/main/java/com/sample/androidexam/core/platform/Constants.kt
@@ -0,0 +1,8 @@
+package com.sample.androidexam.core.platform
+
+object Constants {
+    const val BASE_URL = "https://run.mocky.io/v3/"
+
+    const val STORE = "store"
+    const val PERSONS = "persons"
+}
