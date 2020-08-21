e3afec3c62fb4dc993e440f6b4b3dc2083fb2d69
RJ Mangubat <mangubatrj@gmail.com>
2020-08-21 13:49:50 +0800

    Add proper activity navigation routes

--- app/src/main/java/com/sample/androidexam/core/navigation/Navigator.kt
+++ app/src/main/java/com/sample/androidexam/core/navigation/Navigator.kt
@@ -1,20 +1,9 @@
-/**
- * Copyright (C) 2018 Fernando Cejas Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.sample.androidexam.core.navigation
 
+import android.content.Context
+import com.sample.androidexam.features.persons.Person
+import com.sample.androidexam.features.persons.PersonListActivity
+import com.sample.androidexam.features.persons.details.PersonDetailsActivity
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -23,14 +12,8 @@ import javax.inject.Singleton
 class Navigator
 @Inject constructor() {
 
-   /* private fun showLogin(context: Context) = context.startActivity(LoginActivity.callingIntent(context))
+    fun showPersonList(context: Context) = context.startActivity(PersonListActivity.callingIntent(context))
+    fun showPersonDetails(context: Context, person: Person) = context.startActivity(PersonDetailsActivity.callingIntent(context, person))
 
-    fun showMain(context: Context) {
-        when (authenticator.userLoggedIn()) {
-            true -> showMovies(context)
-            false -> showLogin(context)
-        }
-    }*/
 }
-
 
